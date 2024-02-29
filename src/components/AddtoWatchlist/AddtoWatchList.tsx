b7d8db510c48a4b4b37a2adb7b928fbe326953c2
Zhorakh04 <zhorakh04@gmail.com>
2024-02-29 17:17:14 +0400

    test

--- src/components/AddtoWatchlist/AddtoWatchList.tsx
+++ src/components/AddtoWatchlist/AddtoWatchList.tsx
@@ -3,6 +3,7 @@ import MyButton from "../Ui/customUi/MyButton";
 import "./AddtoWatchlist.css";
 
 const AddtoWatchlist = () => {
+  console.log('test','to be removed')
   return (
     <div className="watchlist">
       <MyButton className={"watchlist_button"}><Star /> Add to Watchlist</MyButton>{" "}
